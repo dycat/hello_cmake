22b3fc832a069a17701df1e8c8b9a2b95a33c28a
dycat <8856528+dycat@users.noreply.github.com>
2022-12-08 17:42:08 +0800

    add lib and use it

--- hello.c
+++ hello.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include "glib.h"
 
 int main() {
-    printf("Hello world.\n");
+    printf("Hello world. Today is fine.\n");
+
+    GList* list = NULL;
+    list = g_list_append(list, "Johnson");
+    printf("The first item is '%s'.\n", g_list_first(list)->data);
 }
